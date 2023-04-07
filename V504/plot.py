4385649b7f64435054f70c6408d1cc9ad49755cd
Fritz Agildere <fritz.agildere@udo.edu>
2023-04-07 18:32:17 +0200

    Auswertung, erster Teil

--- V504/plot.py
+++ V504/plot.py
@@ -46,15 +46,15 @@ err_31 = np.sqrt(np.diag(cov_31))
 par_32, cov_32 = curve_fit(saettigung, U[21:], I_3[21:])
 err_32 = np.sqrt(np.diag(cov_32))
 
-par_4, cov_4 = curve_fit(anlauf, U_a[:-1], I_a[:-1])
+par_4, cov_4 = curve_fit(anlauf, U_a[:-2], I_a[:-2])
 err_4 = np.sqrt(np.diag(cov_4))
 
 # graphical representation of correlation
 plt.plot(np.linspace(20.65,252,23135), saettigung(np.linspace(20.65,252,23135), *par_12), label='Sättigungsstrom')
 plt.plot(np.linspace(-2,19.75,2175), raumladung(np.linspace(-2,19.75,2175), *par_11), label='Raumladungsstrom')
 plt.plot(U, I_1, 'kx', markersize=3.21, label='Messdaten')
-plt.xlabel(r'$U \mathbin{/} \unit{\volt}$')
-plt.ylabel(r'$I \mathbin{/} \unit{\milli\ampere}$')
+plt.xlabel(r'$U_A \mathbin{/} \unit{\volt}$')
+plt.ylabel(r'$I_A \mathbin{/} \unit{\milli\ampere}$')
 plt.legend(loc='lower right')
 plt.savefig('build/plot_1.pdf')
 plt.close()
@@ -62,8 +62,8 @@ plt.close()
 plt.plot(np.linspace(52,252,20000), saettigung(np.linspace(52,252,20000), *par_22), label='Sättigungsstrom')
 plt.plot(np.linspace(-2,48,5000), raumladung(np.linspace(-2,48,5000), *par_21), label='Raumladungsstrom')
 plt.plot(U, I_2, 'kx', markersize=3.21, label='Messdaten')
-plt.xlabel(r'$U \mathbin{/} \unit{\volt}$')
-plt.ylabel(r'$I \mathbin{/} \unit{\milli\ampere}$')
+plt.xlabel(r'$U_A \mathbin{/} \unit{\volt}$')
+plt.ylabel(r'$I_A \mathbin{/} \unit{\milli\ampere}$')
 plt.legend(loc='lower right')
 plt.savefig('build/plot_2.pdf')
 plt.close()
@@ -71,16 +71,16 @@ plt.close()
 plt.plot(np.linspace(102.5,252,14950), saettigung(np.linspace(102.5,252,14950), *par_32), label='Sättigungsstrom')
 plt.plot(np.linspace(-2,97,9900), raumladung(np.linspace(-2,97,9900), *par_31), label='Raumladungsstrom')
 plt.plot(U, I_3, 'kx', markersize=3.21, label='Messdaten')
-plt.xlabel(r'$U \mathbin{/} \unit{\volt}$')
-plt.ylabel(r'$I \mathbin{/} \unit{\milli\ampere}$')
+plt.xlabel(r'$U_A \mathbin{/} \unit{\volt}$')
+plt.ylabel(r'$I_A \mathbin{/} \unit{\milli\ampere}$')
 plt.legend(loc='lower right')
 plt.savefig('build/plot_3.pdf')
 plt.close()
 
 plt.plot(np.linspace(-0.004,0.91,9140), anlauf(np.linspace(-0.004,0.91,9140), *par_4), label='Regression')
 plt.plot(U_a, I_a, 'kx', markersize=3.21, label='Messdaten')
-plt.xlabel(r'$U \mathbin{/} \unit{\volt}$')
-plt.ylabel(r'$I \mathbin{/} \unit{\nano\ampere}$')
+plt.xlabel(r'$U_{-A} \mathbin{/} \unit{\volt}$')
+plt.ylabel(r'$I_A \mathbin{/} \unit{\nano\ampere}$')
 plt.legend(loc='upper right')
 plt.savefig('build/plot_4.pdf')
 plt.close()
@@ -129,11 +129,36 @@ with open('build/table_1.tex', 'w') as f:
 		f.write('\n')
 	f.write(table_footer)
 
+table_header = r'''	\begin{tabular}
+		{S[table-format=1.1, detect-weight, detect-shape, detect-mode]
+		 S[table-format=1.3, detect-weight, detect-shape, detect-mode]
+		 @{\hspace{6ex}}
+		 S[table-format=1.1, detect-weight, detect-shape, detect-mode]
+		 S[table-format=1.3, detect-weight, detect-shape, detect-mode]}
+		\toprule
+		{$U_{-A} \mathbin{/} \unit{\volt}$} &
+		{$I_A \mathbin{/} \unit{\nano\ampere}$} &
+		{$U_{-A} \mathbin{/} \unit{\volt}$} &
+		{$I_A \mathbin{/} \unit{\nano\ampere}$} \\
+		\midrule
+'''
+row_template_0 = r'		{0:1.1f} & {1:} & {2:1.1f} & {3:} \\'
+row_template_1 = r'		{0:1.1f} & {1:} & \bfseries {2:1.1f} & \bfseries {3:} \\'
+with open('build/table_2.tex', 'w') as f:
+	f.write(table_header)
+	for row in zip(U_a[:3], I_a[:3], U_a[5:8], I_a[5:8]):
+		f.write(row_template_0.format(*row))
+		f.write('\n')
+	for row in zip(U_a[3:5], I_a[3:5], U_a[8:10], I_a[8:10]):
+		f.write(row_template_1.format(*row))
+		f.write('\n')
+	f.write(table_footer)
+
 # format and export calculated values to build directory
 with open('build/a_1.tex', 'w') as f:
-	f.write(r'\qty[per-mode=reciprocal]{')
+	f.write(r'\num{')
 	f.write(f'{par_11[0]:.0f}({err_11[0]:.0f})')
-	f.write(r'}{\per\square\meter}')
+	f.write(r'}')
 with open('build/b_1.tex', 'w') as f:
 	f.write(r'\num{')
 	f.write(f'{par_11[1]:.3f}({err_11[1]:.3f})')
@@ -146,7 +171,7 @@ with open('build/v_1.tex', 'w') as f:
 	f.write(r'\qty[per-mode=reciprocal]{')
 	f.write(f'{par_12[1]:.2f}({err_12[1]:.2f})')
 	f.write(r'}{\milli\ampere\volt}\,^{\num{')
-	f.write(f'{par_12[2]:.2f}')
+	f.write(f'{par_12[2]:.2f}({err_12[2]:.2f})')
 	f.write(r'}}')
 with open('build/w_1.tex', 'w') as f:
 	f.write(r'\num{')
@@ -154,9 +179,9 @@ with open('build/w_1.tex', 'w') as f:
 	f.write(r'}')
 
 with open('build/a_2.tex', 'w') as f:
-	f.write(r'\qty[per-mode=reciprocal]{')
+	f.write(r'\num{')
 	f.write(f'{par_21[0]:.0f}({err_21[0]:.0f})')
-	f.write(r'}{\per\square\meter}')
+	f.write(r'}')
 with open('build/b_2.tex', 'w') as f:
 	f.write(r'\num{')
 	f.write(f'{par_21[1]:.3f}({err_21[1]:.3f})')
@@ -169,7 +194,7 @@ with open('build/v_2.tex', 'w') as f:
 	f.write(r'\qty[per-mode=reciprocal]{')
 	f.write(f'{par_22[1]:.1f}({err_22[1]:.1f})')
 	f.write(r'}{\milli\ampere\volt}\,^{\num{')
-	f.write(f'{par_22[2]:.2f}')
+	f.write(f'{par_22[2]:.2f}({err_22[2]:.2f})')
 	f.write(r'}}')
 with open('build/w_2.tex', 'w') as f:
 	f.write(r'\num{')
@@ -177,9 +202,9 @@ with open('build/w_2.tex', 'w') as f:
 	f.write(r'}')
 
 with open('build/a_3.tex', 'w') as f:
-	f.write(r'\qty[per-mode=reciprocal]{')
+	f.write(r'\num{')
 	f.write(f'{par_31[0]:.0f}({err_31[0]:.0f})')
-	f.write(r'}{\per\square\meter}')
+	f.write(r'}')
 with open('build/b_3.tex', 'w') as f:
 	f.write(r'\num{')
 	f.write(f'{par_31[1]:.3f}({err_31[1]:.3f})')
@@ -192,7 +217,7 @@ with open('build/v_3.tex', 'w') as f:
 	f.write(r'\qty[per-mode=reciprocal]{')
 	f.write(f'{par_32[1]:.0f}({err_32[1]:.0f})')
 	f.write(r'}{\milli\ampere\volt}\,^{\num{')
-	f.write(f'{par_32[2]:.2f}')
+	f.write(f'{par_32[2]:.2f}({err_32[2]:.2f})')
 	f.write(r'}}')
 with open('build/w_3.tex', 'w') as f:
 	f.write(r'\num{')
