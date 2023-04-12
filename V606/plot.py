1ba9e24942f5869d7ce018f740aa3073501b381c
Amelie Strathmann <amelie.strathmann@tu-dortmund.de>
2023-04-12 12:18:56 +0200

    irgendwie find ich den fehler nicht

--- V606/plot.py
+++ V606/plot.py
@@ -14,9 +14,29 @@ from uncertainties.unumpy import (nominal_values as noms,   # Wert:             
 # Plot 1:
 
 v, U = np.genfromtxt('data/filterkurve.txt', unpack=True, skip_header=1)      # Normierung 
+n = len(v)                              #the number of data
+mean = sum(v*U)/n                       #note this correction
+# sigma = sum(U*(nu - mean)**2)/n        #note this correction
+sigma = np.sqrt(sum(U*(v - mean)**2))
+
+# Ausgleichsrechung nach Gaußverteilung
+def g(x,a,x0,b):
+    return a*np.exp(-(x-x0)**2/(b)) # b = 2*sigma**2
+
+para, pcov = curve_fit(g, v, U, p0=[1,mean,sigma])
+a, nu0, b = para
+pcov = np.sqrt(np.diag(pcov))
+fa, fnu0, fb = pcov
+
+# Fehler der Parameter
+ua = ufloat(a, fa) 
+ub = ufloat(b, fb)
+unu0 = ufloat(nu0, fnu0)
+
+xx = np.linspace(0, 31, 10**4)
 
 plt.plot(v, U, 'xr', markersize=6 , label = 'Messdaten', alpha=0.5)
-#plt.plot(xx, g(xx, *para), '-b', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
+plt.plot(xx, g(xx, *para), '-b', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
 plt.xlabel(r'$v \, / \, kHz$')
 plt.ylabel(r'$U_A \, / \, U_E v$')
 plt.legend(loc="best")                  # legend position
@@ -28,3 +48,7 @@ plt.savefig('build/plot.pdf', bbox_inches = "tight")
 #plt.show()
 plt.clf() 
 
+#Tabelle der Filterkurve
+#print('Tabelle zur Filterkurve:')
+#filter=pd.read_csv('data/filterkurve.txt',sep=' ', header=None, names=['Frequenz', 'Impulse'])
+#print(filter.to_latex(index=False, column_format="c c"))
