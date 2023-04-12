125270512d757450503f363f27898e9c9a0ee11a
Amelie Strathmann <amelie.strathmann@tu-dortmund.de>
2023-04-12 16:06:46 +0200

    ich habe das gefühl das dauert ewig

--- V606/plot.py
+++ V606/plot.py
@@ -35,7 +35,7 @@ ub = ufloat(b, fb)
 unu0 = ufloat(nu0, fnu0)
 
 xx = np.linspace(0, 31, 10**4)
-print('lalelu')
+
 plt.plot(v, U, 'xr', markersize=6 , label = 'Messdaten', alpha=0.5)
 plt.plot(xx, g(xx, *para), '-b', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
 plt.xlabel(r'$v \, / \, kHz$')
@@ -45,12 +45,39 @@ plt.grid(True)                          # grid style
 #plt.xlim(22, 40)
 #plt.ylim(-0.05, 1.05)
 
-#plt.savefig('build/plot.pdf', bbox_inches = "tight")
-plt.show()
+plt.savefig('build/plot.pdf', bbox_inches = "tight")
+#plt.show()
 plt.clf() 
 
-#Tabelle der Filterkurve
-#print('Tabelle zur Filterkurve:')
-#filter=pd.read_csv('data/filterkurve.txt',sep=' ', header=None, names=['Frequenz', 'Impulse'])
-#print(filter.to_latex(index=False, column_format="c c"))
+# Tabelle der Filterkurve
+# print('Tabelle zur Filterkurve:')
+# filter=pd.read_csv('data/filterkurve.txt',sep=' ', header=None, names=['Frequenz', 'Impulse'])
+# print(filter.to_latex(index=False, column_format="c c"))
 
+#Berechnung des Querschnitts
+#werte derverschiedenen Proben
+
+d_m = 14.38 ; d_l = 16.3 ; d_r =7.8
+g_m = 14.08 ; g_l = 17.3 ; g_r = 7.40
+n_m = 18.48 ; n_l = 14.5 ; n_r = 7.24
+c_m = 7.87 ; c_l = 16.5  
+def Q(m,l,r):
+    return m/(l*r)
+
+d_q = Q(d_m,d_l,d_r)
+g_q = Q(g_m,g_l,g_r)
+n_q = Q(n_m,n_l,n_r)
+
+m = [d_m, g_m, n_m]
+l = [d_l, g_l, n_l]
+rho = [d_r, g_r, n_r]
+Q = np.array([d_q, g_q, n_q])
+stoffe = ['Dy2O3', 'Gd2O3', 'Nd2O3']
+
+f = {'Stoff': stoffe, 'm/g': m, 'l/cm': l, 'rho_w/g/cm**3': rho, 'Q/cm**2': Q}
+df = pd.DataFrame(data = f)
+print(df.to_latex(index = False, column_format= "c c c c", decimal=',')) 
+
+print('Tabelle Dy:')
+dy=pd.read_csv('data/Dy203.txt',sep=' ', header=None, names=['wiederholung', 'ohne', 'mit'])
+print(dy.to_latex(index=False, column_format="c c c"))
