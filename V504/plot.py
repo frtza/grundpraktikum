0c443a7059579fa480c989bd92e567dbd666cc7e
Fritz Agildere <fritz.agildere@udo.edu>
2023-04-18 20:08:21 +0200

    Korrektur

--- V504/plot.py
+++ V504/plot.py
@@ -9,7 +9,11 @@ from uncertainties import ufloat
 
 # read columns of data from txt file
 U, I_1, I_2, I_3 = np.genfromtxt('data/raumladung.txt', unpack=True)
-U_a, I_a = np.genfromtxt('data/anlauf.txt', unpack=True)
+U_af, I_a = np.genfromtxt('data/anlauf.txt', unpack=True)
+
+# correction for internal resistance
+U_a = U_af - 1e6 * I_a * 1e-9
+U_a[0] = 0
 
 # implementation of gaussian filter for smoothing unevenly spaced data
 def gaussian_sum_smooth(x_data, y_data, x_eval, sig):
@@ -82,7 +86,7 @@ plt.close()
 
 plt.plot(np.linspace(-0.004,0.91,9140), anlauf(np.linspace(-0.004,0.91,9140), *par_4), label='Regression')
 plt.plot(U_a, I_a, 'kx', markersize=3.21, label='Messdaten')
-plt.xlabel(r'$U_{-A} \mathbin{/} \unit{\volt}$')
+plt.xlabel(r'$\hat{U}_{-A} \mathbin{/} \unit{\volt}$')
 plt.ylabel(r'$I_A \mathbin{/} \unit{\nano\ampere}$')
 plt.legend(loc='upper right')
 plt.savefig('build/plot_4.pdf')
@@ -135,31 +139,35 @@ with open('build/table_1.tex', 'w') as f:
 table_header = r'''	\begin{tabular}
 		{S[table-format=1.1, detect-weight, detect-shape, detect-mode]
 		 S[table-format=1.3, detect-weight, detect-shape, detect-mode]
-		 @{\hspace{6ex}}
+		 S[table-format=1.6, detect-weight, detect-shape, detect-mode]
+		 @{\hspace{9ex}}
 		 S[table-format=1.1, detect-weight, detect-shape, detect-mode]
-		 S[table-format=1.3, detect-weight, detect-shape, detect-mode]}
+		 S[table-format=1.3, detect-weight, detect-shape, detect-mode]
+		 S[table-format=1.6, detect-weight, detect-shape, detect-mode]}
 		\toprule
 		{$U_{-A} \mathbin{/} \unit{\volt}$} &
 		{$I_A \mathbin{/} \unit{\nano\ampere}$} &
+		{$\hat{U}_{-A} \mathbin{/} \unit{\volt}$} &
 		{$U_{-A} \mathbin{/} \unit{\volt}$} &
-		{$I_A \mathbin{/} \unit{\nano\ampere}$} \\
+		{$I_A \mathbin{/} \unit{\nano\ampere}$} &
+		{$\hat{U}_{-A} \mathbin{/} \unit{\volt}$} \\
 		\midrule
 '''
-row_template_0 = r'		{0:1.1f} & {1:} & {2:1.1f} & {3:} \\'
-row_template_1 = r'		{0:1.1f} & {1:} & \bfseries {2:1.1f} & \bfseries {3:} \\'
+row_template_0 = r'		{0:1.1f} & {1:} & {2:1.6f} & {3:1.1f} & {4:} & {5:1.6f} \\'
+row_template_1 = r'		{0:1.1f} & {1:} & {2:1.6f} & \bfseries {3:1.1f} & \bfseries {4:} & \bfseries {5:1.6f} \\'
 with open('build/table_2.tex', 'w') as f:
 	f.write(table_header)
-	for row in zip(U_a[:3], I_a[:3], U_a[5:8], I_a[5:8]):
+	for row in zip(U_af[:3], I_a[:3], U_a[:3], U_af[5:8], I_a[5:8], U_a[5:8]):
 		f.write(row_template_0.format(*row))
 		f.write('\n')
-	for row in zip(U_a[3:5], I_a[3:5], U_a[8:10], I_a[8:10]):
+	for row in zip(U_af[3:5], I_a[3:5], U_a[3:5], U_af[8:10], I_a[8:10], U_a[8:10]):
 		f.write(row_template_1.format(*row))
 		f.write('\n')
 	f.write(table_footer)
 
 N_WL = 1.0
-I_H = np.array([2.0, 2.2, 2.4])
-U_H = np.array([3.5, 4.5, 5.0])
+I_H = unp.uarray([2.0, 2.2, 2.4], [0.1, 0.1, 0.1])
+U_H = unp.uarray([3.5, 4.5, 5.0], [1, 1, 1])
 T_H = ((I_H*U_H - N_WL)/(0.32*0.28*5.7e-12))**(1/4)
 I_S = unp.uarray([par_12[0], par_22[0], par_32[0]], [err_12[0], err_22[0], err_32[0]]) * 1e-3
 W_J = -k_B * T_H * unp.log((I_S * h_0**3)/(4*np.pi * e_0 * m_0 * 0.000032 * k_B**2 * T_H**2))
@@ -167,30 +175,36 @@ W_eV = W_J / e_0
 W_w = ufloat(sum(noms(W_eV)/(stds(W_eV)**2))/sum(1/(stds(W_eV)**2)), np.sqrt(1/sum(1/(stds(W_eV)**2))))
 table_header = r'''	\begin{tabular}
 		{S[table-format=1.1]
+		 @{${}\pm{}$}
+		 S[table-format=1.1]
+		 S[table-format=1.1]
+		 @{${}\pm{}$}
 		 S[table-format=1.1]
 		 S[table-format=4.0]
+		 @{${}\pm{}$}
+		 S[table-format=1.0]
 		 S[table-format=1.3]
 		 @{${}\pm{}$}
 		 S[table-format=1.3]
 		 S[table-format=3.1]
 		 @{${}\pm{}$}
-		 S[table-format=1.1, table-number-alignment=left]
+		 S[table-format=2.1, table-number-alignment=left]
 		 S[table-format=1.3]
 		 @{${}\pm{}$}
 		 S[table-format=1.3]}
 		\toprule
-		{$I_H \mathbin{/} \unit{\ampere}$} &
-		{$U_H \mathbin{/} \unit{\volt}$} &
-		{$T \mathbin{/} \unit{\kelvin}$} &
+		\multicolumn{2}{c}{$I_H \mathbin{/} \unit{\ampere}$} &
+		\multicolumn{2}{c}{$U_H \mathbin{/} \unit{\volt}$} &
+		\multicolumn{2}{c}{$T \mathbin{/} \unit{\kelvin}$} &
 		\multicolumn{2}{c}{$I_S \mathbin{/} \unit{\milli\ampere}$} &
 		\multicolumn{2}{c}{$W \mathbin{/} \qty{e-21}{\joule}$} &
 		\multicolumn{2}{c}{$W \mathbin{/} \unit{\electronvolt}$} \\
 		\midrule
 '''
-row_template = r'		{0:1.1f} & {1:1.1f} & {2:4.0f} & {3:1.3f} & {4:1.3f} & {5:3.1f} & {6:1.1f} & {7:1.3f} & {8:1.3f} \\'
+row_template = r'		{0:1.1f} & {1:1.1f} & {2:1.1f} & {3:1.1f} & {4:4.0f} & {5:1.0} & {6:1.3f} & {7:1.3f} & {8:3.1f} & {9:2.1f} & {10:1.3f} & {11:1.3f} \\'
 with open('build/table_3.tex', 'w') as f:
 	f.write(table_header)
-	for row in zip(I_H, U_H, T_H, noms(I_S) * 1e3, stds(I_S) * 1e3, noms(W_J) * 1e21, stds(W_J) * 1e21, noms(W_eV), stds(W_eV)):
+	for row in zip(noms(I_H), stds(I_H), noms(U_H), stds(U_H), noms(T_H), stds(T_H), noms(I_S) * 1e3, stds(I_S) * 1e3, noms(W_J) * 1e21, stds(W_J) * 1e21, noms(W_eV), stds(W_eV)):
 		f.write(row_template.format(*row))
 		f.write('\n')
 	f.write(table_footer)
