b0340a5c1f6f9aeaed8d7bfc572c334b8b0254cb
Fritz Agildere <fritz.agildere@udo.edu>
2023-04-18 23:39:30 +0200

    Formatieren und Einlesen

--- V701/plot.py
+++ V701/plot.py
@@ -4,6 +4,9 @@ from matplotlib.ticker import AutoMinorLocator
 import numpy as np
 
 # read columns of data from txt file
+p_6, qty_6, max_6, chnl_6 = np.genfromtxt('data/stats_6cm.txt', unpack=True)
+p_4, qty_4, max_4, chnl_4 = np.genfromtxt('data/stats_4cm.txt', unpack=True)
+stat = np.genfromtxt('data/qty_4cm_300mbar.txt', unpack=True)
 
 # parameter and error arrays, covariance matrix for regression
 
