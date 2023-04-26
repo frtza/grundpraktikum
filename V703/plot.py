7295f136fc9d59c766837b6097b2167095cc16ca
Amelie Strathmann <amelie.strathmann@tu-dortmund.de>
2023-04-26 11:35:33 +0200

    angefangeb

--- V703/plot.py
+++ V703/plot.py
@@ -1,14 +1,26 @@
-# essential libraries
 import matplotlib.pyplot as plt
-from matplotlib.ticker import AutoMinorLocator
+import matplotlib as mpl
 import numpy as np
+from numpy import sqrt
+import pandas as pd
+import scipy.constants as const
+from scipy.optimize import curve_fit                        # Funktionsfit:     popt, pcov = curve_fit(func, xdata, ydata) 
+from uncertainties import ufloat                            # Fehler:           fehlerwert =  ulfaot(x, err)
+from uncertainties import unumpy as unp 
+from uncertainties.unumpy import uarray                     # Array von Fehler: fehlerarray =  uarray(array, errarray)
+from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
+                                  std_devs as stds)         # Abweichung:       stds(fehlerarray) = errarray
 
-# read columns of data from txt file
+#latexTabelle der Messdaten zur Kennlinie des Geiger-Müller-Zählrohrs
+print('Tabelle zur Krnnlinie:')
+kennlinie=pd.read_csv('data/kennlinie.txt',sep=' ', header=None, names=['Spannung', 'Zählrate', 'Strom'])
+print(kennlinie.to_latex(index=False, column_format="c c c"))
 
-# parameter and error arrays, covariance matrix for regression
+#Kennlinie Geiger-Müller
 
-# graphical representation of correlation
+U, N, I = np.genfromtxt('data/kennlinie.txt', unpack=True, skip_header=1)   
 
-# format and export latex table
 
-# format and export calculated values to build directory
+
+
+
