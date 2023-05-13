20eba20f95b3b2c723dfb812cc7a8b0c172fbd79
Amelie Strathmann <amelie.strathmann@tu-dortmund.de>
2023-05-13 21:33:07 +0200

    das wird eine lange nacht

--- V503/plot.py
+++ V503/plot.py
@@ -1,20 +1,46 @@
-# essential libraries
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
+from numpy import sqrt
+import pandas as pd
 import scipy.constants as const
-from scipy.optimize import curve_fit
-import uncertainties.unumpy as unp
-from uncertainties.unumpy import nominal_values as noms, std_devs as stds
-from uncertainties import ufloat
+from scipy.optimize import curve_fit                        # Funktionsfit:     popt, pcov = curve_fit(func, xdata, ydata) 
+from uncertainties import ufloat                            # Fehler:           fehlerwert =  ulfaot(x, err)
+from uncertainties import unumpy as unp 
+from uncertainties.unumpy import uarray                     # Array von Fehler: fehlerarray =  uarray(array, errarray)
+from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
+                                  std_devs as stds)  
+#Bedingung für die Gültigkeit der Messwerte prüfen
+#Fubntion der Bedingung 
+def bed(v_0,v_ab,v_auf):
+    return ((2 * v_0) - (v_ab - v_auf)) / (v_ab - v_auf)
 
-# read columns of data from txt file
- 
-#Konstanten definieren
+#strecke
+s = ufloat(0.5, 0.1) #mm
 
-#berechnung von v
+#daten einlesen
+tab, tauf, t0 = np.genfromtxt('data/spannung1/1.txt', unpack=True, skip_header=1)
+#mitteln der Daten
+print('t_auf:')
+mean_auf = np.mean(tauf)
+std_auf = np.std(tauf)
+t_mittel_auf = ufloat(mean_auf,std_auf)
+print(t_mittel_auf)
 
-#Feldstärke kondensator
+print('t_ab:')
+mean_ab = np.mean(tab)
+std_ab = np.std(tab)
+t_mittel_ab = ufloat(mean_ab,std_ab)
+print(t_mittel_ab)
+#Berechnung von v
+# v= s/t
+def v(z,d):
+    return z/d 
+vauf = v(s,t_mittel_auf)
+vab = v(s,t_mittel_ab)
+v0 = v(s,t0)
 
-#Radius der öltröpchen
-
-
+print('Bedingung prüfen:')
+#ver für verhältnis
+ver = bed(v0,vab,vauf)
+print(ver)
