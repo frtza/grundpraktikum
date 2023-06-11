9f9f1f99f45235623db11eaef6d0a6392eb4d0c2
Amelie Strathmann <amelie.strathmann@tu-dortmund.de>
2023-06-11 14:58:24 +0200

    ich bin dumm

--- US1/plot.py
+++ US1/plot.py
@@ -20,15 +20,25 @@ acryl=pd.read_csv('data/acryl.txt', sep=' ', header=None, names=['n','t'])
 
 n, t = np.genfromtxt('data/acryl.txt', unpack=True, skip_header=1) 
 n2, ak, bk, d,f = np.genfromtxt('data/ausmessung.txt', unpack=True , skip_header=1)
+
+acryl=pd.read_csv('data/acryl.txt', sep=' ', header=None, names=['n','t'])
+#print(acryl.to_latex(index=False, column_format="c c"))
+
+#acryl a scan
+
+n, t = np.genfromtxt('data/acryl.txt', unpack=True, skip_header=1) 
+n2, ak, bk, d,f = np.genfromtxt('data/ausmessung.txt', unpack=True , skip_header=1)
 d = d * 10**(-3)
+ak = ak * 10**(-3)
+bk = bk * 10**(-3)
 t = t * 10**(-6)
 
-plt.plot(t,2*d[1:8], 'xr', markersize=6 , label = 'Messdaten')
+plt.plot(t,2*ak[1:8], 'xr', markersize=6 , label = 'Messdaten')
 
 def g(x, a, b):
     return a * x + b
 
-para, pcov = curve_fit(g, 2*d[1:8], t)
+para, pcov = curve_fit(g, 2*ak[1:8], t)
 a, b = para
 fa, fb = np.sqrt(np.diag(pcov))
 ua = ufloat(a, fa) 
